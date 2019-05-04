f6009beaefc2fec674ecb6b43607e8174331f4e3
CesarCedanoV <CesarCedanoV@gmail.com>
2019-05-04 17:14:37 -0400

    Added the MONGO_URI and also a mongoose.plugin to avoid a pushAll error

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://dbadmin:dbadmin123@ds151486.mlab.com:51486/lyric_match';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
